ea5f4f1a51cc4153d9aad7e79cd166e1b57b9992
Zhuang Ma <chumpma@gmail.com>
2016-05-10 01:52:15 +0800

    Init with sogou search engine

---
+++ more-search-engines.safariextension/global.js
@@ -0,0 +1,14 @@
+engines = {
+  sogou: {title:'Sogou', url: 'https://www.sogou.com/web?query=@@@&ie=utf8'}
+}
+
+safari.application.addEventListener('beforeSearch', function(e) {
+  e.preventDefault();
+  var url = engines['sogou'].url;
+  url = url.replace('@@@', encodeURIComponent(e.query).replace(/%20/g, '+'));
+  e.target.url = url;
+}, false);
+
+//safari.application.addEventListener("command", function(e) {
+//  safari.extension.settings.engine = e.command;
+//}, false);
